587719ac79c6b01bf6c5da200b6b5f24e22f3a66
Ben Hansen <bhbenjaminhansen@gmail.com>
2020-10-30 15:20:56 -0600

    modified setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,12 @@
-RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack build
+echo "Compiling wasm..."
+RUSTFLAGS=--cfg=web_sys_unstable_apis wasm-pack -vv build
 
 # TODO: figure out how to use yarn workspaces with wasm
+echo "Setting up `yarn link` to generated wasm package..."
 cd pkg
 yarn link
 cd ..
 yarn link "wgpu-multiplatform"
 
+echo "Installing dependencies..."
 yarn install
